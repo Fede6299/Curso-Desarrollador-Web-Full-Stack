2f56d2bfc7440be9b1ce734073b627ecfbe4fe7e
Fede6299 <fapaza@frba.utn.edu.ar>
2024-11-05 01:01:33 -0300

    Tarea: Falta punto c.

--- Tareas/11-01-Clase-7/PHP-Arrays-y-Funciones/index.php
+++ Tareas/11-01-Clase-7/PHP-Arrays-y-Funciones/index.php
@@ -38,5 +38,42 @@ foreach ($ciudadesConIndice as $key => $value) {
 }
 
 //2. Funciones
+function calcular_perimetro_superficie($lado){
+    echo "Perímetro del cuadrado basado en un lado dado: " .  4 * $lado  . "cm<br>Superficie del cuadrado basado en un lado dado: " . $lado * $lado . "cm²<br>"; ;
+}
+calcular_perimetro_superficie(10);
+
+echo "<br>";
+function cadena_mayuscula($cadena){
+    echo strtoupper($cadena);
+}
+cadena_mayuscula("chiquito");
+
+echo "<br>";
+
+function cadena_minuscula($cadena){
+    echo strtolower($cadena);
+}
+cadena_minuscula("GRANDE");
+
+//C. DUDA SI INGRESA 2 COMO MES QUE DEBERÍA RETORNA SI ES 30 O 31
+
+echo "<br>";
+
+function palindrome($cadena){
+    $cadena_sin_espacios = str_replace(' ', '', $cadena);
+    $cadena_sin_espacios = strtolower($cadena_sin_espacios);
+    $cadenaInversa = "";
+    $longitud = strlen($cadena_sin_espacios) - 1;
+    for ($i = 0; $i < strlen($cadena_sin_espacios); $i++) {
+        $cadenaInversa [$i] = $cadena_sin_espacios[$longitud];
+        $longitud -= 1;
+    }
+    return $cadena_sin_espacios === $cadenaInversa;
+}
+
+echo "<br>";
+
+echo palindrome("Anita lava la tina") ? "Es un palindrome" : "No es un palindrome";
 
 ?>
